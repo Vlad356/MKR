42575408974cd672ba660d3148ada2f8bfd5aaaa
Vlad356 <vladstadniychuk@gmail.com>
2023-11-20 00:30:12 +0200

    Book interface, MySQL Database

--- demo/src/main/java/com/libraryproject/models/Book.java
+++ demo/src/main/java/com/libraryproject/models/Book.java
@@ -1,12 +1,34 @@
 package com.libraryproject.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "BOOK")
 public class Book {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "title")
     private String title;
+    @Column(name = "author")
     private String author;
+    @Column(name = "description")
     private String description;
+    @Column(name = "publisher")
     private String publisher;
+    @Column(name = "isbn")
     private String isbn;
+    @Column(name = "year")
     private Integer year;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getTitle(){
         return title;
@@ -54,5 +76,7 @@ public class Book {
     public void setYear(Integer year) {
         this.year = year;
     }
+
+
 }
 
--- demo/src/main/java/com/libraryproject/services/BookService.java
+++ demo/src/main/java/com/libraryproject/services/BookService.java
@@ -1,33 +1,58 @@
 package com.libraryproject.services;
 
 import com.libraryproject.models.Book;
+import com.libraryproject.models.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
     public Book get(Long id){
-        // Return book from database
-        return new Book();
+        Optional<Book> byId = bookRepository.findById(id);
+
+        if (byId.isPresent())
+        {
+            return byId.get();
+        }
+        throw new RuntimeException("Book not found");
     }
 
     public List<Book> getAll(){
-        // get all the books from database
-        return Collections.emptyList();
+        Iterator<Book> iterator = bookRepository.findAll().iterator();
+
+        List<Book> books = new ArrayList<>();
+
+        while (iterator.hasNext()){
+            books.add(iterator.next());
+        }
+        
+        return books;
     }
 
     public Book create(Book book){
-        // Save book to the database
-        return book;
+        Book save = bookRepository.save(book);
+        return save;
     }
 
     public Book update (Long id, Book book){
-        // get book by id and update it
-        return book;
+        Book original = get(id);
+        original.setTitle(book.getTitle());
+        original.setAuthor(book.getAuthor());
+        original.setDescription(book.getDescription());
+        original.setPublisher(book.getPublisher());
+        original.setIsbn(book.getIsbn());
+        original.setYear(book.getYear());
+
+        Book updated = bookRepository.save(original);
+        return updated;
     }
     public void delete(Long id){
-        // delete book from database
+        Book book = get(id);
+        bookRepository.delete(book);
     }
 }
