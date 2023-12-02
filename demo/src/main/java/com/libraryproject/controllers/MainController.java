a35ba14385ab191a2f5626d5d0b7b27573d2002d
Vlad356 <vladstadniychuk@gmail.com>
2023-12-02 21:37:55 +0200

    Main page

--- demo/src/main/java/com/libraryproject/controllers/MainController.java
+++ demo/src/main/java/com/libraryproject/controllers/MainController.java
@@ -2,7 +2,6 @@ package com.libraryproject.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
 
 @Controller
 public class MainController {
